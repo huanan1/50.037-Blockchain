f58f9d411567111224600d2b151021e5d7aaf4f8
Xiang Hao <tanxianghao@hotmail.com>
2020-02-26 16:04:41 +0800

    Created SPVClient class

---
+++ wk 4/SPVClient.py
@@ -0,0 +1,19 @@
+class SPVClient:
+    def __init__(self, node_id = ''):
+        return None
+    
+    def associate_keys(self):
+        return None
+
+    def receive_block_header(self):
+        return None
+
+    def receive_transaction(self):
+        return None
+
+    def verify_transaction(self):
+        return None
+
+    def send_transactions(self):
+        return None
+    
