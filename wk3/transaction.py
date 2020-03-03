49bf9e3feb809407869af4dcab3a9c35d10545b0
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2020-03-03 16:18:34 +0800

    clean up comments

--- wk3/transaction.py
+++ wk3/transaction.py
@@ -6,7 +6,6 @@ import binascii
 class Transaction:
 
     def __init__(self, sender, receiver, amount, comment=''):
-        # Instantiates object from passed values
         self.sender = sender  # public key
         self.receiver = receiver  # public key
         assert amount > 0
@@ -15,7 +14,9 @@ class Transaction:
         self.sign()
 
     def to_json(self):
-        # Serializes object to JSON string
+        '''
+        Serializes object to JSON string
+        '''
         json_dict = self.__dict__
         json_dict['sender'] = json_dict['sender'].to_string().hex()
         json_dict['receiver'] = json_dict['receiver'].to_string().hex()
@@ -23,7 +24,9 @@ class Transaction:
 
     @classmethod
     def from_json(cls, json_str):
-        # Instantiates/Deserializes object from JSON string
+        '''
+        Instantiates/Deserializes object from JSON string
+        '''
         trans = json.loads(json_str)
         transaction = Transaction(SigningKey.from_string(binascii.unhexlify(bytes(trans['sender'], 'utf-8'))), SigningKey.from_string(
             binascii.unhexlify(bytes(trans['receiver'], 'utf-8'))), trans['amount'], trans['comment'])
@@ -33,13 +36,12 @@ class Transaction:
         return str(self.sender.to_string()) + str(self.receiver.to_string()) + str(self.amount) + self.comment
 
     def sign(self):
-        # Sign object with private key passed
-        # That can be called within new()
         self.sender.sign(self.transaction_to_string().encode())
 
     def validate(self, trans):
-        # Validate transaction correctness.
-        # Can be called within from_json()
+        '''
+        Validate transaction correctness
+        '''
         sender_vk = self.sender.verifying_key
         assert sender_vk.verify(self.sender.sign(
             trans.encode()), self.transaction_to_string().encode())
