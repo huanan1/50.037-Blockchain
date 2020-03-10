e9d8f7c3b82a694b8c831e9be4d46c2623c05e50
Kenneth Sim <kensim28@hotmail.com>
2020-03-11 02:10:57 +0800

    added todo comments

--- wk3/build_miners_local_automation.py
+++ wk3/build_miners_local_automation.py
@@ -2,7 +2,8 @@ import os
 import copy
 import time
 
-# define the name of the directory to be created
+# This file is only for local testing, if using multiple comps, don't use
+# Reads LOCAL ports to use via miner_ports.txt
 f = open("miner_ports.txt", "r")
 list_of_miner_ports = []
 list_of_miner_ips = []
@@ -12,14 +13,17 @@ for line in f:
         "127.0.0.1:" + line)
 f.close()
 
+# Color args
 colors = ['w','r','g','y','b','m','c']
-
 for count, i in enumerate(list_of_miner_ports):
     list_of_partner_miners = copy.deepcopy(list_of_miner_ips)
     del list_of_partner_miners[count]
+    # Creates a file called partner_miner_ip.txt
     f = open("partner_miner_ip.txt", "w+")
     f.writelines(list_of_partner_miners)
     f.close()
+    # Reads file
     os.system("python3 miner.py -p {0} -i partner_miner_ip.txt -c {1} -m 2&".format(i, colors[count%len(colors)]))
-    time.sleep(2) if count == 0 else time.sleep(2)
+    time.sleep(2)
+    # Removes file for cleanup
     os.system('rm partner_miner_ip.txt')
--- wk3/miner.py
+++ wk3/miner.py
@@ -4,31 +4,35 @@ from merkle_tree import MerkleTree
 import random
 import time
 import copy
-from flask import Flask, request
-from multiprocessing import Process, Queue
 import pickle
 import requests
 import sys
 import getopt
 import colorama
+from flask import Flask, request
+from multiprocessing import Process, Queue
+
 
 app = Flask(__name__)
 
-# Parsing arguments when 
+# Parsing arguments when entered via CLI
+# miner.py -p <port> -i <inputfile of list of IPs of other miners> -c <color w|r|h|y|m|c> -m <mode 1/2>
 def parse_arguments(argv):
     inputfile = ''
     outputfile = ''
     color = ''
-    list_of_miner_ip=[]
+    list_of_miner_ip = []
     try:
-        opts, args = getopt.getopt(argv, "hp:i:c:m:", ["port=", "ifile=","color=","mode="])
+        opts, args = getopt.getopt(
+            argv, "hp:i:c:m:", ["port=", "ifile=", "color=", "mode="])
+    # Only port and input is mandatory
     except getopt.GetoptError:
-        print ('test.py -p<port> -i <inputfile of list of IPs of other miners> -c <color w|r|h|y|m|c> -m <mode 1/2>')
+        print('miner.py -p <port> -i <inputfile of list of IPs of other miners> -c <color w|r|h|y|m|c> -m <mode 1/2>')
         sys.exit(2)
     for opt, arg in opts:
         mode = 1
         if opt == '-h':
-            print ('test.py -p<port> -i <inputfile of list of IPs of other miners> -c <color w|r|h|y|m|c> -m <mode 1/2>')
+            print('miner.py -p <port> -i <inputfile of list of IPs of other miners> -c <color w|r|h|y|m|c> -m <mode 1/2>')
             sys.exit()
         elif opt in ("-p", "--port"):
             my_port = arg
@@ -62,9 +66,13 @@ def parse_arguments(argv):
                 mode = 1
     return my_port, list_of_miner_ip, color, mode
 
+
+# Get data from arguments
 MY_PORT, LIST_OF_MINER_IP, COLOR, MODE = parse_arguments(sys.argv[1:])
 # MY_IP will be a single string in the form of "127.0.0.1:5000"
 # LIST_OF_MINER_IP will be a list of strings in the form of ["127.0.0.1:5000","127.0.0.1:5001","127.0.0.1:5002"]
+# COLOR is color of text using colorama library
+# MODE is either 1 or 2, 1 is full details, 2 is shortform
 
 
 class Miner:
@@ -81,19 +89,20 @@ class Miner:
             self.reset_new_mine()
             # print(MY_IP)
             return True
+        # Increase nonce everytime the mine fails
         self.nonce += 1
-        # print(block.header_hash())
         return False
 
     def reset_new_mine(self):
         self.nonce = 0
         self.current_time = str(time.time())
         self.blockchain.resolve()
-        # need to include proper checks when new block is added
-        # check should be in here or resolve?
-        # self.blockchain.difficulty_adjust()
+        # resets after every block is added, both network and locally
 
+    # Called when a new block is received by another miner
+    # TODO Huan An, here is where the add is called, network_add should verify the block
     def network_block(self, block):
+        # Checks if the n
         if self.blockchain.network_add(block):
             self.reset_new_mine()
 
@@ -106,7 +115,21 @@ def create_sample_merkle():
     merkletree.build()
     return merkletree
 
+# TODO Youngmin, so erm, it's a bit complex regarding the ledger
+# There has to be a copy of the ledger at every single block
+# This is to ensure that Huan An's validation code can work
+# I am not sure if you want to do a class, or a text file or something
+# I suggest putting the ledger in the Block object, so every Block has a copy
+# of the ledger, then when the new transactions come, you can pull out the latest
+# block, using Blockchain.last_block() will return you the latest block, so you will
+# have the latest ledger to be used here in the merkle_tree
 
+# There are 2 main checks to be done here
+# 1. The accounts involved have enough money to transact, depending on the ledger
+# 2. TXID (hash of transaction, not created yet) is not duplicated. I can't think of a way other than looking through EVERY transaction
+
+# Creates a merkle tree by compiling all of the transactions in transaction_queue
+# Sends the merkle tree to be made into a Block object
 def create_merkle(transaction_queue):
     list_of_raw_transactions = []
     list_of_validated_transactions = []
@@ -114,7 +137,7 @@ def create_merkle(transaction_queue):
         list_of_raw_transactions.append(
             Transaction.from_json(transaction_queue.get()))
     for transaction in list_of_raw_transactions:
-        # TODO: check if transaction is okay
+        # TODO: check if transaction makes sense in the ledger
         if True:
             list_of_validated_transactions.append(transaction)
 
@@ -132,31 +155,45 @@ def start_mining(block_queue, transaction_queue):
     blockchain = BlockChain()
     miner = Miner(blockchain)
     miner_status = False
+    # Infinite loop
     while True:
         while True:
+            # Mines the once every round
             miner_status = miner.mine(merkletree)
+            mine_or_recv = ""
+            # Check if that mine is successful
             if miner_status:
                 sending_block = blockchain.last_block()
+                # Grab the last block and send to network
                 data = pickle.dumps(sending_block, protocol=2)
                 for miner_ip in LIST_OF_MINER_IP:
                     send_failed = True
+                    mine_or_recv = "Block MINED\n"
+                    # Retry until peer receives, idk i think prof say ok right? assume all in stable network lel
                     while send_failed:
                         try:
-                            requests.post("http://"+miner_ip+"/block", data=data)
+                            requests.post("http://"+miner_ip +
+                                          "/block", data=data)
                             send_failed = False
                         except:
                             time.sleep(0.1)
                 break
             # Checks value of nonce, as checking queue every cycle makes it very laggy
             if miner.nonce % 10000 == 0:
+                # Check if new blocks have been detected
                 if not block_queue.empty():
+                    mine_or_recv = "Block RECEIVED\n"
+                    # If detected, add new block to blockchain
+                    # TODO add rebroadcast of signal??
                     new_block = block_queue.get()
                     miner.network_block(new_block)
                     break
         # Section run if the miner found a block or receives a block that has been broadcasted
-        print(COLOR + (str(miner.blockchain) if MODE==1 else str(miner.blockchain).split("~~~")[1]))
+        print(COLOR + "PORT: {}\n".format(MY_PORT) + mine_or_recv +
+              (str(miner.blockchain) if MODE == 1 else str(miner.blockchain).split("~~~\n")[1]))
         # merkletree = create_merkle(transaction_queue)
 
+# Queue objects for passing stuff between processes
 block_queue = Queue()
 transaction_queue = Queue()
 
@@ -170,6 +207,7 @@ def new_block_network():
 @app.route('/transaction')
 def new_transaction_network():
     # Needs to add a proper transaction object, currently thing will fail
+    # TODO add rebroadcast of signal??
     transaction_queue.put("a")
 
 
