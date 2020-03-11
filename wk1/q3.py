91a9ad911491958085a95ff98cb2321cb9164512
Kensim48 <kensim28@hotmail.com>
2020-03-11 13:18:41 +0800

    branch from master

--- wk1/q3.py
+++
@@ -1,6 +0,0 @@
-import ecdsa
-
-private = ecdsa.SigningKey.generate()
-public = private.get_verifying_key()
-sig = private.sign(b"Blockchain Technology")
-print(public.verify(sig, b"Blockchain Technology")) # True
