a01db2d41c2b64655b2d8970a9d7f2fe76e8fd20
huanan1 <huanan_cheng@mymail.sutd.edu.sg>
2020-03-13 18:32:27 +0800

    added time, txid and sig field to transaction object

--- transaction.py
+++ transaction.py
@@ -1,25 +1,42 @@
 import json
 from ecdsa import SigningKey
+import hashlib
 import binascii
+import time
 
 
 class Transaction:
 
-    def __init__(self, sender, receiver, amount, comment=''):
+    def __init__(self, sender, receiver, amount, comment="", time=time.time(), txid="", sig=b""):
         self.sender = sender  # public key
         self.receiver = receiver  # public key
         assert amount > 0
         self.amount = amount
         self.comment = comment
-        self.sign()
+        self.time = time
+        self.txid = self.generate_txid()
+        if sig == b"":
+            self.sig = self.sign()
+        else:
+            self.sig = sig
+
+    def generate_txid(self):
+        m = hashlib.sha256()
+        m.update((str(self.sender.to_string()) + str(self.receiver.to_string()) + str(self.amount) + self.comment + str(self.time)).encode())
+        return m.hexdigest()
 
     def to_json(self):
         '''
         Serializes object to JSON string
         '''
-        json_dict = self.__dict__
-        json_dict['sender'] = json_dict['sender'].to_string().hex()
-        json_dict['receiver'] = json_dict['receiver'].to_string().hex()
+        json_dict = dict()
+        json_dict['sender'] = self.sender.to_string().hex()
+        json_dict['receiver'] = self.receiver.to_string().hex()
+        json_dict['amount'] = str(self.amount)
+        json_dict['comment'] = self.comment
+        json_dict['time'] = str(self.time)
+        json_dict['txid'] = str(self.txid)
+        json_dict['sig'] = str(self.sig)
         return json.dumps(json_dict)
 
     @classmethod
@@ -29,30 +46,37 @@ class Transaction:
         '''
         trans = json.loads(json_str)
         transaction = Transaction(SigningKey.from_string(binascii.unhexlify(bytes(trans['sender'], 'utf-8'))), SigningKey.from_string(
-            binascii.unhexlify(bytes(trans['receiver'], 'utf-8'))), trans['amount'], trans['comment'])
+            binascii.unhexlify(bytes(trans['receiver'], 'utf-8'))), int(trans['amount']), trans['comment'], float(trans['time']), trans['txid'], trans['sig'].encode())
         return transaction
 
     def transaction_to_string(self):
-        return str(self.sender.to_string()) + str(self.receiver.to_string()) + str(self.amount) + self.comment
+        '''
+        Convert the fields in the current transaction object to string
+        '''
+        return self.sender.to_string().hex() + self.receiver.to_string().hex() + str(self.amount) + self.comment + str(self.txid) + time.asctime(time.localtime(self.time))
 
     def sign(self):
-        self.sender.sign(self.transaction_to_string().encode())
+        '''
+        Sign the encoded (byte-form) version of transaction object
+        '''
+        return self.sender.sign(self.transaction_to_string().encode())
 
-    def validate(self, trans):
+    def validate(self, signature):
         '''
         Validate transaction correctness
+        Compare signature (signed transaction object in bytes) and transaction object in bytes
+        i.e. s(b) and b
         '''
         sender_vk = self.sender.verifying_key
-        assert sender_vk.verify(self.sender.sign(
-            trans.encode()), self.transaction_to_string().encode())
+        assert sender_vk.verify(signature, self.transaction_to_string().encode())
 
     def __str__(self):
         return self.transaction_to_string()
 
     def __eq__(self, other):
         # Check if all parts of the transaction are equal
-        return(self.sender == other.sender and self.receiver == other.receiver and self.amount == other.amount and self.comment == other.comment)
-
+        return(self.sender == other.sender and self.receiver == other.receiver and self.amount == other.amount and self.comment == other.comment and 
+                self.txid == other.txid and self.time == other.time)
 
 # sender = SigningKey.generate()
 
@@ -62,3 +86,9 @@ class Transaction:
 # t1 = Transaction(sender, receiver, 100)
 # t1_json = t1.to_json()
 # t1_back = Transaction.from_json(t1_json)
+
+# print(t1_json)
+# print(t1)
+# print(t1_back)
+# print(t1==t1_back)
+# t1.validate(t1.sig)
